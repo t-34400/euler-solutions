789f59e0b5e68f01a63761029f3eb0cc38f7676c
Takumi Miyoshi <34400mt@gmail.com>
2023-05-09 00:42:37 +0900

    Add Python solution for Problem 2

--- 1-50/2/answer.py
+++ 1-50/2/answer.py
@@ -3,14 +3,10 @@ limit_value = 4_000_000
 previous_value = 0
 current_value = 1
 sum_of_values = 0
-while True:
-    next_value = previous_value + current_value
-    if next_value % 2 == 0:
-        sum_of_values += next_value
-    if next_value >= limit_value:
-        break
+while current_value < limit_value:
+    if current_value % 2 == 0:
+        sum_of_values += current_value
 
-    previous_value = current_value
-    current_value = next_value
+    previous_value, current_value = current_value, previous_value + current_value
 
 print(sum_of_values)
